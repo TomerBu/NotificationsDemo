4804918b385d649eeacf11c3abd581be7819dffa
tomerbuzaglo <tomer.bu@gmail.com>
2016-10-26 10:56:04 +0300

    changed nothing

--- Demo/app/src/main/java/tomerbu/edu/demo/DeepLinkReferActivity.java
+++ Demo/app/src/main/java/tomerbu/edu/demo/DeepLinkReferActivity.java
@@ -5,36 +5,48 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.google.android.gms.appinvite.AppInvite;
 import com.google.android.gms.appinvite.AppInviteReferral;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class DeepLinkReferActivity extends AppCompatActivity {
 
     private String TAG = "Tomerbu";
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deep_link_refer);
 
-       // AppInvite.AppInviteApi.getInvitation(mGoogleApiClient, null, autoLaunchDeepLink);
-        // Check if the intent contains an AppInvite and then process the referral information.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, null /* OnConnectionFailedListener */)
+
+                .addApi(AppInvite.API)
+                .build();
+
+        boolean autoLaunchDeepLink = true;
+
         Intent intent = getIntent();
         if (AppInviteReferral.hasReferral(intent)) {
             processReferralIntent(intent);
         }
 
-
     }
+
+
     private void processReferralIntent(Intent intent) {
+        boolean autoLaunchDeepLink = false;
+        AppInvite.AppInviteApi.getInvitation(mGoogleApiClient, this, autoLaunchDeepLink);
+
         // Extract referral information from the intent
         String invitationId = AppInviteReferral.getInvitationId(intent);
         String deepLink = AppInviteReferral.getDeepLink(intent);
 
         // Display referral information
         // [START_EXCLUDE]
-        Log.d(TAG, "Found Referral: " + invitationId + ":" + deepLink);
-        Log.d(TAG, "Found Referral: " + invitationId + ":" + invitationId);
+        Log.e(TAG, "Found Referral: " + invitationId + ":" + deepLink);
+        Log.e(TAG, "Found Referral: " + invitationId + ":" + invitationId);
 
     }
-
 }
